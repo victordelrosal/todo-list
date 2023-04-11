2d85f197f28b434aa4f49b6d8860bb6102d62e7a
victordelrosal <victordelrosal@gmail.com>
2023-04-11 20:24:39 +0100

    firebase commit finally

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-    https://todo-list-6e32f-default-rtdb.europe-west1.firebasedatabase.app/
-};
+    apiKey: "AIzaSyBwkF4bXrZp2M3MkcloPnI_IQYoPDpVik4",
+    authDomain: "todo-list-6e32f.firebaseapp.com",
+    databaseURL: "https://todo-list-6e32f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todo-list-6e32f",
+    storageBucket: "todo-list-6e32f.appspot.com",
+    messagingSenderId: "645532534713",
+    appId: "1:645532534713:web:fdb0aaf5c7ab10e3d21868"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
