0fe019f1080a9e0283fea4553424eeaa7ff26727
victordelrosal <victordelrosal@gmail.com>
2023-04-11 20:04:52 +0100

    a ver

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { database } from './firebase';
 import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
 
+  useEffect(() => {
+    const todosRef = database.ref('todos');
+    todosRef.on('value', (snapshot) => {
+      const data = snapshot.val();
+      const todoItems = data ? Object.values(data) : [];
+      setTodos(todoItems);
+    });
+  }, []);
+
   const addTodo = (e) => {
     e.preventDefault();
-    setTodos([...todos, input]);
+
+    const newTodo = {
+      id: Date.now(),
+      content: input,
+    };
+
+    database.ref(`todos/${newTodo.id}`).set(newTodo);
+
     setInput('');
   };
 
@@ -19,13 +36,13 @@ function App() {
           value={input}
           onChange={(e) => setInput(e.target.value)}
           type="text"
-          placeholder="Add a newer task"
+          placeholder="Add a new task"
         />
         <button type="submit">Add</button>
       </form>
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
         ))}
       </ul>
     </div>
